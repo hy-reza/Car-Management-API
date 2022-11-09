03340dd1bbf85c14094792a56e28cbb401eb6abf
hy-reza <hy.rezaalfanda@gmail.com>
2022-11-09 16:01:35 +0700

    production

--- db/config/config.js
+++ db/config/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_URL } =
+  process.env;
 
 module.exports = {
   development: {
@@ -9,5 +10,11 @@ module.exports = {
     database: DB_NAME,
     host: DB_HOST,
     dialect: DB_DIALECT,
+  },
+  production: {
+    url: DB_URL,
+    dialect: DB_DIALECT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
   },
 };
